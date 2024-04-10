3967eedaf5d1063f162fe369c53e8c59703c3c3f
Leandro Martins <leandro.marttins.85@gmail.com>
2024-04-09 22:16:58 -0300

    609 - Logfn Form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
+import LoginForm from '@/components/LoginForm'
+
 export default async function LoginPage() {
   return (
-    <main>
-      <h1>Login</h1>
-    </main>
+    <section>
+      <h1 className="title">Login</h1>
+      <LoginForm />
+    </section>
   )
 }
