9dc660c437a92b3557f8de7fcbbce8677c3f3a8c
Leandro Martins <leandro.marttins.85@gmail.com>
2024-04-23 16:36:53 -0300

    618 - Create context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,31 @@ import './globals.css'
 import { typeSecond } from './functions/fonts'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import userGet from '@/actions/user-get'
+import { UserContextProvider } from '@/context/user-context'
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
   description: 'Rede social para cachorros',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { data: user } = await userGet()
+
   return (
     <html lang="pt-BR" className={typeSecond.variable}>
       <body>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   )
