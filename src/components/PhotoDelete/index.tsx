e9023e64c2fe077217b1a3d79dd74e66bef10fc6
Leandro Martins <leandro.martins.85@gmail.com>
2024-05-23 17:06:39 -0300

    Photo Delete

--- src/components/PhotoDelete/index.tsx
+++ src/components/PhotoDelete/index.tsx
@@ -1,3 +1,35 @@
-export default async function PhotoDelete({ id }: { id: string }) {
-  return <button>Deletar: {id}</button>
+'use client'
+
+import photoDelete from '@/actions/photo-delete'
+import { useState } from 'react'
+
+import styles from './photo-delete.module.css'
+
+type PhotoDeleteProps = {
+  id: string
+}
+export default function PhotoDelete({ id }: PhotoDeleteProps) {
+  const [loading, setLoading] = useState(false)
+
+  async function handleClick() {
+    setLoading(true)
+    const confirm = window.confirm('Tem certeza que deseja deletar?')
+    if (confirm) {
+      await photoDelete(id)
+    }
+    setLoading(false)
+  }
+  return (
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button className={styles.delete} onClick={handleClick}>
+          Deletar
+        </button>
+      )}
+    </>
+  )
 }
